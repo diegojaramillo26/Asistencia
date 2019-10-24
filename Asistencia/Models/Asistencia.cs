9a18df18dc6ddb3aaece7340e730f6dd2abf605a
Diego Jaramillo <djevolucion@gmail.com>
2019-10-24 16:12:02 -0500

    Commit inicial

---
+++ Asistencia/Models/Asistencia.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Asistencia.Models
+{
+    public class Asistencia
+    {
+        [UIHint("SignaturePad")]
+        public byte[] MySignature { get; set; }
+    }
+}
